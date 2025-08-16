3330a9c0edc6f691fc3a9bf9cda6ab59c5e03dbe
roney ruiz <roneyruizrojas@gmail.com>
2025-08-16 09:30:59 -0600

    feat: example of tkinter window

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,15 @@
-from Database.Peewee import db
+""" from Database.Peewee import db
 from Database.Migrator import Migrator
 
 migrator = Migrator(db)
-migrator.migrate()
+migrator.migrate() """
+
+import tkinter as tk
+
+window = tk.Tk()
+
+window.geometry("1200x600")
+window.title("Familiar Tree")
+window.resizable(False, False)
+
+window.mainloop()
