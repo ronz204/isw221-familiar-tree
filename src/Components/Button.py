35ccd2480b3df9c971d2f58415ef05096f1492e5
roney ruiz <roneyruizrojas@gmail.com>
2025-08-16 17:31:15 -0600

    feat: create button component

---
+++ src/Components/Button.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from Enums.Pallete import Pallete
+
+class Button(tk.Button):
+  def __init__(self, widget: tk.Widget, text: str):
+    super().__init__(widget, text=text, **STYLES)
+
+STYLES = {
+  "cursor": "hand2",
+  "highlightthickness": 0,
+  "font": ("Arial", 12, "bold"),
+
+  "bd": 0,
+  "pady": 0,
+  "padx": 12,
+  "width": 20,
+  "height": 2,
+  "anchor": "w",
+
+  "bg": Pallete.GREEN_200.value,
+  "fg": Pallete.WHITE_100.value,
+  "activebackground": Pallete.GREEN_100.value,
+  "activeforeground": Pallete.WHITE_100.value,
+}
