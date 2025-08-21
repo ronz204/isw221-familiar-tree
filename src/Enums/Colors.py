25a89ecf507c4d7f7a3100c562fb2baa8fcaf92e
roney ruiz <roneyruizrojas@gmail.com>
2025-08-20 18:39:32 -0600

    feat: add new colors to pallete

--- src/Enums/Colors.py
+++ src/Enums/Colors.py
@@ -1,7 +1,11 @@
 from enum import Enum
 
 class Colors(Enum):
+  GRAY_100 = "#f0f0f0"
+  GRAY_200 = "#555555"
   GREEN_100 = "#34495E"
   GREEN_200 = "#2C3E50"
   WHITE_100 = "#FFFFFF"
   WHITE_200 = "#ECF0F1"
+  GREEN_300 = "#4CAF50"
+  GREEN_400 = "#45a049"
