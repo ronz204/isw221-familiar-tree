14324fd7e6e2924c1b5024ab0bcae6e0f2c5d9b5
roney ruiz <roneyruizrojas@gmail.com>
2025-08-21 21:08:37 -0600

    chore: remove relation view

--- src/Routing/Routes.py
+++ src/Routing/Routes.py
@@ -1,9 +1,7 @@
 from enum import Enum
 from Views.FamilyView import FamilyView
 from Views.MemberView import MemberView
-from Views.RelationView import RelationView
 
 class Routes(Enum):
   FAMILY = FamilyView
   MEMBER = MemberView
-  RELATION = RelationView
