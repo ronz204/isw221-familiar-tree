895243d0b1245441183bed340cebba15a9fc09e9
roney ruiz <roneyruizrojas@gmail.com>
2025-08-23 23:46:20 -0600

    chore: remove old files

--- src/Events/Family/FamilyCreatedEvent.py
+++
@@ -1,5 +0,0 @@
-from Events.Event import Event
-
-class FamilyCreatedEvent(Event):
-  def __init__(self, data = None):
-    super().__init__("family_created", data)
--- src/Handlers/Family/CreateFamily/CreateFamilySchema.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel, Field
-
-class CreateFamilySchema(BaseModel):
-  name: str = Field(min_length=2, max_length=20)
