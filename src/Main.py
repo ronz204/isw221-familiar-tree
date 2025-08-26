8345fb25b9024c64504cb545eb138c1df326b1a2
roney ruiz <roneyruizrojas@gmail.com>
2025-08-25 23:01:05 -0600

    feat: create schedule helper

--- src/Main.py
+++ src/Main.py
@@ -5,8 +5,13 @@ from Screens.FamilyScreen import FamilyScreen
 from Screens.PersonScreen import PersonScreen
 from Screens.RelateScreen import RelateScreen
 
+from Helpers.ScheduleHelper import ScheduleHelper
+from Handlers.Person.Birthdate.BirthdateHandler import BirthdateHandler
+from Handlers.Person.Deathdate.DeathdateHandler import DeathdateHandler
+
 window = tk.Tk()
 broker = Broker()
+scheduler = ScheduleHelper()
 
 window.geometry("1200x600")
 window.title("Familiar Tree")
@@ -20,4 +25,12 @@ notebook.add(FamilyScreen(notebook, broker), text="Familia")
 notebook.add(PersonScreen(notebook, broker), text="Persona")
 notebook.add(RelateScreen(notebook, broker), text="Relaciones")
 
+scheduler.start(BirthdateHandler(broker), 10, "birthdate")
+# scheduler.start(DeathdateHandler(broker), 15, "deathdate")
+
+def on_closing():
+  scheduler.stop_all()
+  window.destroy()
+
+window.protocol("WM_DELETE_WINDOW", on_closing)
 window.mainloop()
