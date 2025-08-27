f6a96740006566890d65204253772facb6825dbb
roney ruiz <roneyruizrojas@gmail.com>
2025-08-27 16:05:05 -0600

    chore: remove old files

--- src/Handlers/Person/RelateCouple/RelateCoupleEvent.py
+++
@@ -1,8 +0,0 @@
-from Events.Event import Event
-
-class RelateCoupleEvent(Event):
-  name = "relate_couple"
-  label = "Relate Couple"
-
-  def __init__(self, data = None):
-    super().__init__(data)
--- src/Handlers/Person/RelateCouple/RelateCoupleSchema.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel, Field
-
-class RelateCoupleSchema(BaseModel):
-  person1_id: int = Field(gt=0)
-  person2_id: int = Field(gt=0)
