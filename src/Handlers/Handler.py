582f3ef1184bbab306e1087d116afb62d4031ec7
roney ruiz <roneyruizrojas@gmail.com>
2025-08-26 21:28:46 -0600

    refac: add typing and validate method in base handler

--- src/Handlers/Handler.py
+++ src/Handlers/Handler.py
@@ -1,8 +1,20 @@
 from Events.Broker import Broker
+from pydantic import BaseModel, ValidationError
+from typing import Dict, Any, Optional, TypeVar, Generic
 
-class Handler:
-  def __init__(self, broker: Broker):
+Schema = TypeVar("Schema", bound=BaseModel)
+
+class Handler(Generic[Schema]):
+  def __init__(self, broker: Broker, schema: Schema):
     self.broker: Broker = broker
+    self.schema: Schema = schema
 
-  def execute(self) -> None:
+  def validate(self, data: Dict[str, Any]) -> Optional[Schema]:
+    try:
+      return self.schema(**data)
+    except ValidationError as e:
+      print(f"Validation error: {e}")
+      return None
+
+  def execute(self, data: Dict[str, Any]) -> None:
     raise NotImplementedError("This method should be overridden by subclasses")
