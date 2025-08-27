31a6c12aa7932ad19034cc590600e22d3bfb1846
roney ruiz <roneyruizrojas@gmail.com>
2025-08-26 21:28:22 -0600

    chore: remove old files

--- src/Events/Person/BirthdateEvent.py
+++
@@ -1,5 +0,0 @@
-from Events.Event import Event
-
-class BirthdateEvent(Event):
-  def __init__(self, data = None):
-    super().__init__("birthdate", data)
--- src/Events/Person/DeathdateEvent.py
+++
@@ -1,5 +0,0 @@
-from Events.Event import Event
-
-class DeathdateEvent(Event):
-  def __init__(self, data = None):
-    super().__init__("deathdate", data)
