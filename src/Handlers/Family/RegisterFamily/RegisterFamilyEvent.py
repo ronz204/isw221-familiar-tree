53887e941bf924dc2f08758f7cdb3aeff9b46cc8
roney ruiz <roneyruizrojas@gmail.com>
2025-08-27 17:33:32 -0600

    chore: purge family relation files

--- src/Handlers/Family/RegisterFamily/RegisterFamilyEvent.py
+++
@@ -1,8 +0,0 @@
-from Events.Event import Event
-
-class RegisterFamilyEvent(Event):
-  name = "register_family"
-  label = "Register Family"
-
-  def __init__(self, data = None):
-    super().__init__(data)
