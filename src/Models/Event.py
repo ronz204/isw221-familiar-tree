846bf8403661b966333b9e9df10a0951a12c0958
roney ruiz <roneyruizrojas@gmail.com>
2025-08-26 23:17:22 -0600

    feat: create event and timeline models

---
+++ src/Models/Event.py
@@ -0,0 +1,11 @@
+from Database.Peewee import db
+from peewee import Model, AutoField, CharField
+
+class Event(Model):
+  id = AutoField(primary_key=True)
+  name = CharField(unique=True, max_length=100)
+  label = CharField(max_length=100)
+
+  class Meta:
+    database = db
+    table_name = "Event"
--- src/Models/Timeline.py
+++ src/Models/Timeline.py
@@ -1,12 +1,14 @@
 import datetime as dt
 from Database.Peewee import db
+from Models.Event import Event
 from Models.Person import Person
-from peewee import Model, AutoField, ForeignKeyField, CharField, DateTimeField
+from peewee import Model, AutoField, ForeignKeyField, IntegerField, TimestampField
 
 class Timeline(Model):
   id = AutoField(primary_key=True)
-  event = CharField(max_length=255)
-  timestamp = DateTimeField(default=dt.datetime.now)
+  year = IntegerField()
+  timestamp = TimestampField(default=dt.datetime.now)
+  event = ForeignKeyField(Event, backref="timelines", on_delete="CASCADE")
   person = ForeignKeyField(Person, backref="timelines", on_delete="CASCADE")
 
   class Meta:
