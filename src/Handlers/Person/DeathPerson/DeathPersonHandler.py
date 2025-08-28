a76d7b783d656125816d512cb5684d3242aad3fb
roney ruiz <roneyruizrojas@gmail.com>
2025-08-27 23:04:24 -0600

    feat: add widowed couple hanlding

--- src/Handlers/Person/DeathPerson/DeathPersonHandler.py
+++ src/Handlers/Person/DeathPerson/DeathPersonHandler.py
@@ -6,9 +6,12 @@ from Events.Broker import Broker
 from typing import Dict, Any, List
 from Handlers.Handler import Handler
 from Models.Timeline import Timeline
+from Models.Relation import Relation
+from Models.Enums.Status import Status
 
 from Handlers.Person.DeathPerson.DeathPersonEvent import DeathPersonEvent
 from Handlers.Person.DeathPerson.DeathPersonSchema import DeathPersonSchema
+from Handlers.Person.DeathPerson.WidowedPersonEvent import WidowedPersonEvent
 
 class DeathPersonHandler(Handler[DeathPersonSchema]):
   def __init__(self, broker: Broker):
@@ -23,11 +26,14 @@ class DeathPersonHandler(Handler[DeathPersonSchema]):
     for person in people:
       if random.random() < 0.05:
         person.deathdate = datetime.now()
+        person.status = Status.DEATHED.value
+        
+        year = person.deathdate.year
         person.save()
 
-        year = person.deathdate.year
-        event = Event.get(Event.name == DeathPersonEvent.name)
-        Timeline.create(person_id=person.id, event_id=event.id, year=year)
+        self.handle_widowed_couple(person, year)
+        death_event = Event.get(Event.name == DeathPersonEvent.name)
+        Timeline.create(person_id=person.id, event_id=death_event.id, year=year)
 
         self.broker.publish(DeathPersonEvent({
           "person_id": person.id,
@@ -35,3 +41,14 @@ class DeathPersonHandler(Handler[DeathPersonSchema]):
           "person_name": person.name,
           "deathdate": person.deathdate.isoformat()
         }))
+  
+  def handle_widowed_couple(self, person: Person, year: int):
+    relation = Relation.get_or_none((Relation.man == person) | (Relation.woman == person))
+    if not relation: return
+
+    couple = relation.woman if relation.man == person else relation.man
+    couple.status = Status.WIDOWED.value
+    couple.save()
+
+    widowed_event = Event.get(Event.name == WidowedPersonEvent.name)
+    Timeline.create(person_id=couple.id, event_id=widowed_event.id, year=year)
