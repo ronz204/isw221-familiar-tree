e599c124f8d7d6ee890791c3c305caa79d01cf5e
roney ruiz <roneyruizrojas@gmail.com>
2025-08-31 13:01:27 -0600

    feat: complete register person handler

---
+++ src/Application/Handlers/RegisterPerson/RegisterPersonHandler.py
@@ -0,0 +1,31 @@
+from Domain.Models.Event import Event
+from Domain.Enums.Status import Status
+from Domain.Models.Person import Person
+from Domain.Models.Timeline import Timeline
+
+from Application.Events.Broker import Broker
+from Application.Handlers.Handler import Handler
+from Application.Events.Person.PersonBornEvent import PersonBornEvent
+from Application.Handlers.RegisterPerson.RegisterPersonSchema import RegisterPersonSchema
+
+class RegisterPersonHandler(Handler[RegisterPersonSchema]):
+  def __init__(self, broker: Broker):
+    super().__init__(broker, RegisterPersonSchema)
+
+  def process(self, validated: RegisterPersonSchema) -> None:
+    data = validated.model_dump()
+
+    if data.get("deathdate"):
+      data["status"] = Status.DEATHED.value
+    
+    person, created = Person.get_or_create(**data)
+    if not created: return
+
+    event = Event.get(Event.name == PersonBornEvent.name)
+    Timeline.create(event=event, person=person, timestamp=data["birthdate"])
+
+    self.broker.publish(PersonBornEvent({
+      "id": person.id,
+      "name": person.name,
+      "cedula": person.cedula,
+    }))
