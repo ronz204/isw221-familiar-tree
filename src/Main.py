5bda9cbc6d77ed582c0f24cc49888072e10d06a4
roney ruiz <roneyruizrojas@gmail.com>
2025-08-31 14:38:06 -0600

    feat: create relate screen and builder

--- src/Main.py
+++ src/Main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from Application.Events.Broker import Broker
 from Presentation.Screens.Person.PersonScreen import PersonScreen
+from Presentation.Screens.Relate.RelateScreen import RelateScreen
 
 window = tk.Tk()
 broker = Broker()
@@ -15,5 +16,6 @@ notebook = ttk.Notebook(window)
 notebook.pack(fill="both", expand=True)
 
 notebook.add(PersonScreen(notebook, broker), text="Registrar Personas")
+notebook.add(RelateScreen(notebook, broker), text="Relacionar Personas")
 
 window.mainloop()
---
+++ src/Presentation/Screens/Relate/RelateScreen.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+from typing import Dict, Any
+from Application.Events.Bus import Bus
+from Application.Events.Event import Event
+from Application.Events.Broker import Broker
+from Application.Events.Listener import Listener
+
+from Presentation.Screens.Relate.RelateBuilder import RelateBuilder
+from Application.Handlers.RelatePeople.RelatePeopleHandler import RelatePeopleHandler
+
+from Application.Events.Person.RelatedPeopleEvent import RelatedPeopleEvent
+from Application.Events.Person.RegisteredPersonEvent import RegisteredPersonEvent
+
+class RelateScreen(tk.Frame, Listener):
+  def __init__(self, parent: tk.Widget, broker: Broker):
+    super().__init__(parent)
+    self.broker: Broker = broker
+    self.bus: Bus = Bus()
+
+    self.builder = RelateBuilder(self)
+    self.relate_people_handler = RelatePeopleHandler(broker)
+
+    self.setup_ui_components()
+    self.subscribe_to_events()
+
+  def subscribe_to_events(self):
+    self.broker.subscribe(RelatedPeopleEvent.name, self)
+    self.broker.subscribe(RegisteredPersonEvent.name, self)
+
+    self.bus.add(RelatedPeopleEvent.name, self.on_related_people)
+    self.bus.add(RegisteredPersonEvent.name, self.on_registered_person)
+
+  def setup_ui_components(self):
+    self.builder.setup_grid()
+    self.builder.build_container()
+    self.builder.build_frames()
+    self.builder.build_title()
+
+    self.builder.build_person_field()
+    self.builder.build_father_field()
+    self.builder.build_mother_field()
+    self.builder.build_guardian_field()
+
+    self.builder.build_relate_button(self.relate_command)
+    self.builder.build_clear_button(self.clear_command)
+
+    self.builder.load_data_hydration()
+
+  def relate_command(self):
+    data = self.builder.get_form_data()
+    self.relate_people_handler.handle(data)
+
+  def clear_command(self):
+    self.builder.clear_form_fields()
+
+  def listen(self, event: Event):
+    self.bus.get(event.name)(event.data)
+
+  def on_related_people(self, data: Dict[str, Any]):
+    self.builder.clear_form_fields()
+
+  def on_registered_person(self, data: Dict[str, Any]):
+    self.builder.load_data_hydration()
