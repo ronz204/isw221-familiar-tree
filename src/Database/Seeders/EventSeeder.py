862c4aa50a70a9101b93caf2c2462a75cb208efe
roney ruiz <roneyruizrojas@gmail.com>
2025-08-31 14:37:37 -0600

    feat: update event seeder

--- src/Database/Seeders/EventSeeder.py
+++ src/Database/Seeders/EventSeeder.py
@@ -4,12 +4,14 @@ from Application.Events.Event import Event
 from Domain.Models.Event import Event as Model
 
 from Application.Events.Person.PersonBornEvent import PersonBornEvent
+from Application.Events.Person.RelatedPeopleEvent import RelatedPeopleEvent
 from Application.Events.Person.RegisteredPersonEvent import RegisteredPersonEvent
 
 class EventSeeder(Seeder):
   def seed(self) -> None:
     events: List[Event] = [
       PersonBornEvent,
+      RelatedPeopleEvent,
       RegisteredPersonEvent,
     ]
 
