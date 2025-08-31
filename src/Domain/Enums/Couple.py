1876d3f47b1557c2521e2f90bc1c606b44f3a1bc
roney ruiz <roneyruizrojas@gmail.com>
2025-08-31 14:56:33 -0600

    feat: add status to relations

---
+++ src/Domain/Enums/Couple.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class Couple(Enum):
+  CURRENT = "C"
+  PREVIOUS = "P"
--- src/Domain/Models/Relation.py
+++ src/Domain/Models/Relation.py
@@ -1,11 +1,14 @@
 from Database.Core.Peewee import db
 from Domain.Models.Person import Person
-from peewee import Model, AutoField, ForeignKeyField, TimestampField
+from peewee import Model, AutoField, ForeignKeyField, TimestampField, CharField
+
+STATUS_CHOICES = [("C", "Current"), ("P", "Previous")]
 
 class Relation(Model):
   id = AutoField(primary_key=True)
   timestamp = TimestampField()
-
+  
+  status = CharField(max_length=20, choices=STATUS_CHOICES, default="C")
   man = ForeignKeyField(Person, backref="relations_as_man", on_delete="CASCADE")
   woman = ForeignKeyField(Person, backref="relations_as_woman", on_delete="CASCADE")
 
