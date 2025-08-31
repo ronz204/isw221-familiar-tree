12dde08139ec0f3ffc84fa11623a4176700c8a30
roney ruiz <roneyruizrojas@gmail.com>
2025-08-31 13:00:38 -0600

    chore: change timestamp data type field

--- src/Domain/Models/Timeline.py
+++ src/Domain/Models/Timeline.py
@@ -1,11 +1,11 @@
 from Database.Core.Peewee import db
 from Domain.Models.Event import Event
 from Domain.Models.Person import Person
-from peewee import Model, AutoField, ForeignKeyField, TimestampField
+from peewee import Model, AutoField, ForeignKeyField, DateField
 
 class Timeline(Model):
   id = AutoField(primary_key=True)
-  timestamp = TimestampField()
+  timestamp = DateField()
 
   event = ForeignKeyField(Event, backref="timelines", on_delete="CASCADE")
   person = ForeignKeyField(Person, backref="timelines", on_delete="CASCADE")
