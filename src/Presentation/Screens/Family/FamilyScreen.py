18a0998a60b2e29ab9e1c4c9e94934c8137ac477
roney ruiz <roneyruizrojas@gmail.com>
2025-08-31 18:05:36 -0600

    feat: complete family screen and builder

--- src/Presentation/Screens/Family/FamilyScreen.py
+++ src/Presentation/Screens/Family/FamilyScreen.py
@@ -1,7 +1,5 @@
 import tkinter as tk
-from typing import Dict, Any
 from Application.Events.Bus import Bus
-from Domain.Models.Person import Person
 from Application.Events.Event import Event
 from Application.Events.Broker import Broker
 from Application.Events.Listener import Listener
@@ -24,7 +22,12 @@ class FamilyScreen(tk.Frame, Listener):
     self.builder.build_canvas()
     self.builder.build_scrollers()
 
+    self.builder.build_legend_info()
+    self.setup_family_tree()
+
+  def setup_family_tree(self):
     self.builder.load_data_hydration()
+    self.builder.build_family_tree()
 
   def listen(self, event: Event):
     self.bus.get(event.name)(event.data)
