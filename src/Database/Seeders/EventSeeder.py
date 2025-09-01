403f3a74e8c28984888111d2e79912e6b0ec3615
roney ruiz <roneyruizrojas@gmail.com>
2025-09-01 12:09:09 -0600

    feat: create recent births found event

--- src/Database/Seeders/EventSeeder.py
+++ src/Database/Seeders/EventSeeder.py
@@ -12,6 +12,7 @@ from Application.Events.Person.DeathedPersonEvent import DeathedPersonEvent
 from Application.Events.Person.WidowedPersonEvent import WidowedPersonEvent
 from Application.Events.Person.RelativesFoundEvent import RelativesFoundEvent
 from Application.Events.Person.RegisteredPersonEvent import RegisteredPersonEvent
+from Application.Events.Person.RecentBirthsFoundEvent import RecentBirthsFoundEvent
 from Application.Events.Person.DeathedPeopleFoundEvent import DeathedPeopleFoundEvent
 from Application.Events.Person.ChildrenTogetherFoundEvent import ChildrenTogetherFoundEvent
 from Application.Events.Person.DescendantsFoundAliveEvent import DescendantsFoundAliveEvent
@@ -29,6 +30,7 @@ class EventSeeder(Seeder):
       WidowedPersonEvent,
       RelativesFoundEvent,
       RegisteredPersonEvent,
+      RecentBirthsFoundEvent,
       DeathedPeopleFoundEvent,
       ChildrenTogetherFoundEvent,
       DescendantsFoundAliveEvent,
