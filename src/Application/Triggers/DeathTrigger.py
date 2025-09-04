70d9ac2e349163710e47ceabc883f41b705e2707
roney ruiz <roneyruizrojas@gmail.com>
2025-09-03 19:53:06 -0600

    feat: add algt to calcule who death

--- src/Application/Triggers/DeathTrigger.py
+++ src/Application/Triggers/DeathTrigger.py
@@ -1,5 +1,6 @@
 import random
 import datetime
+from typing import List
 from Domain.Models.Event import Event
 from Domain.Enums.Status import Status
 from Domain.Models.Person import Person
@@ -14,17 +15,30 @@ class DeathTrigger(Trigger):
     super().__init__(broker)
 
   def trigger(self):
-    people = Person.select().where(Person.deathdate.is_null() & (Person.emotional <= 70))
-    person = random.choice(people)
+    people = Person.select().where(Person.deathdate.is_null())
+    if not people: return
+    
+    selected_person = self.weighted_random_selection(people)
+    if not selected_person:return
 
-    person.deathdate = datetime.datetime.now()
-    person.status = Status.DEATHED.value
-    person.save()
+    selected_person.deathdate = datetime.datetime.now()
+    selected_person.status = Status.DEATHED.value
+    selected_person.save()
 
     event = Event.get(Event.name == DeathedPersonEvent.name)
-    Timeline.create(person=person, event=event, timestamp=person.deathdate)
+    Timeline.create(person=selected_person, event=event, timestamp=selected_person.deathdate)
 
     self.broker.publish(DeathedPersonEvent({
-      "id": person.id,
-      "timestamp": person.deathdate,
+      "id": selected_person.id,
+      "timestamp": selected_person.deathdate,
     }))
+
+  def weighted_random_selection(self, people: List[Person]):
+    if not people: return None
+    weights = []
+
+    for person in people:
+      weight = 101 - person.emotional
+      weights.append(weight)
+  
+    return random.choices(people, weights=weights, k=1)[0]
