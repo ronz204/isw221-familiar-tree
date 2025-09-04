fb682917f21404f1dccdaedae51a58842039b40e
roney ruiz <roneyruizrojas@gmail.com>
2025-09-03 18:42:00 -0600

    feat: handle family relationship

--- src/Application/Handlers/MatchPeople/MatchPeopleHandler.py
+++ src/Application/Handlers/MatchPeople/MatchPeopleHandler.py
@@ -3,7 +3,8 @@ from Domain.Enums.Status import Status
 from Domain.Models.Person import Person
 from Domain.Models.Relation import Relation
 from Domain.Models.Timeline import Timeline
-from Domain.Models.Passions import Passions
+from Domain.Genealogy.GenealogyTypes import GenealogyTypes
+from Domain.Genealogy.GenealogyAnalyzer import GenealogyAnalyzer
 
 from Application.Events.Broker import Broker
 from Application.Handlers.Handler import Handler
@@ -13,6 +14,7 @@ from Application.Handlers.MatchPeople.MatchPeopleSchema import MatchPeopleSchema
 class MatchPeopleHandler(Handler[MatchPeopleSchema]):
   def __init__(self, broker: Broker):
     super().__init__(broker, MatchPeopleSchema)
+    self.genealogy_analyzer = GenealogyAnalyzer()
 
   def process(self, validated: MatchPeopleSchema) -> None:
     man: Person = Person.get_by_id(validated.man_id)
@@ -23,7 +25,9 @@ class MatchPeopleHandler(Handler[MatchPeopleSchema]):
 
     if abs(man.age - woman.age) > 15: return
     if man.age < 18 or woman.age < 18: return
+    
     if not self.are_compatible(man, woman): return
+    if self.has_family_relationship(man, woman): return
 
     Relation.create(man=man, woman=woman, timestamp=validated.timestamp)
 
@@ -60,3 +64,8 @@ class MatchPeopleHandler(Handler[MatchPeopleSchema]):
     
     if compatibility_percentage >= 70: return True
     return False
+
+  def has_family_relationship(self, man: Person, woman: Person) -> bool:
+    genealogy_report = self.genealogy_analyzer.analyze(man, woman)
+    if genealogy_report.relationship == GenealogyTypes.NO_RELATION: return False
+    return True
